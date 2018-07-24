ad9cd06f9632445e4d1966d66dbd1c842e2aa2ab
James <jbean46@gmail.com>
2018-07-23 22:04:12 -0700

    Remove unused nil comparison overloads

--- Sources/Math/PowerGenerator.swift
+++ Sources/Math/PowerGenerator.swift
@@ -58,23 +58,3 @@ public class PowerGenerator <T: FixedWidthInteger>: IteratorProtocol {
         return result <= max ? result : nil
     }
 }
-
-func < <T: Comparable> (lhs: T?, rhs: T?) -> Bool {
-    switch (lhs, rhs) {
-    case let (l?, r?):
-        return l < r
-    case (nil, _?):
-        return true
-    default:
-        return false
-    }
-}
-
-func > <T: Comparable> (lhs: T?, rhs: T?) -> Bool {
-    switch (lhs, rhs) {
-    case let (l?, r?):
-        return l > r
-    default:
-        return rhs < lhs
-    }
-}
