0e5a38ac6f996156021dd68eca60430eb2c1fad9
James <jbean46@gmail.com>
2018-08-08 23:22:42 -0700

    Refine quadratic doc comments and implementation

--- Sources/Math/Quadratic.swift
+++ Sources/Math/Quadratic.swift
@@ -8,18 +8,12 @@
 
 import Darwin
 
+/// - Returns: A set of 0, 1, or 2 results of the quadratic equation for the given coefficients.
 public func quadratic <T: FloatingPoint> (_ a: T, _ b: T, _ c: T) -> Set<T> {
-    
     let discriminant = b * b - 4 * a * c
-    
-    guard discriminant > 0 else {
-        return Set<T>()
-    }
-    
+    guard discriminant > 0 else { return [] }
     let denominator = 2 * a
-    
     let pos: T = (-b + sqrt(discriminant)) / denominator
     let neg: T = (-b - sqrt(discriminant)) / denominator
-    
-    return Set<T>([pos,neg])
+    return [pos,neg]
 }
