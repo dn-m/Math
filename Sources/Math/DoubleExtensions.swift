58d6c5e5d5703c755d9ed8b26684173f9a8499c5
James <jbean46@gmail.com>
2018-08-08 22:55:59 -0700

    Add warnings about generalizing exponential scale(d) over FloatingPoint

--- Sources/Math/DoubleExtensions.swift
+++ Sources/Math/DoubleExtensions.swift
@@ -12,6 +12,11 @@ extension Double {
 
     /// Exponentially scales a `Double` from the given `source` to the given
     /// `destination`.
+    ///
+    #warning("""
+    Abstract Double.scale(from:to:) over `FloatingPoint`. This requires writing a `pow(_:)` which
+    is generic over `FloatingPoint` values.
+    """)
     public mutating func scale(
         from source: ClosedRange<Double>,
         toExponential destination: ClosedRange<Double>
@@ -25,6 +30,11 @@ extension Double {
 
     /// - returns: A `Double` value scaled from the given `source` range to the
     /// given `destination` range.
+    ///
+    #warning("""
+    Abstract Double.scaled(from:to:) over `FloatingPoint`. This requires writing a `pow(_:)` which
+    is generic over `FloatingPoint` values.
+    """)
     public func scaled(
         from source: ClosedRange<Double>,
         toExponential destination: ClosedRange<Double>
