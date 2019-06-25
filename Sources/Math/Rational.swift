ea889423ac63bbe268539b9de1f394658c9e7624
James Bean <bean@Jamess-Mac-mini.local>
2019-06-24 22:12:28 -0400

    Refine doc comment

--- Sources/Math/Rational.swift
+++ Sources/Math/Rational.swift
@@ -293,7 +293,8 @@ extension Rational {
 
     /// Hash value.
     ///
-    /// - Note: `Rational` values are hashed by their `floatValue`. That is to say that `1/3` and `2/6` are equivalent.
+    /// - Note: `Rational` values are hashed by their `floatValue`. That is to say that `1/3` and `2/6` are hashed
+    /// equivalently.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(floatValue)
     }
