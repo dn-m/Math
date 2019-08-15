c100c168f63687020e5233f9cc8f94bbd8f1ad6e
James Bean <jbean46@gmail.com>
2019-08-15 14:32:56 +0200

    Add more Swift toolchain versions to Travis CI (#35)

--- Sources/Math/DoubleExtensions.swift
+++ Sources/Math/DoubleExtensions.swift
@@ -6,13 +6,52 @@
 //  Copyright © 2017 James Bean. All rights reserved.
 //
 
+// Only expose these functions before Swift 5.1
+#if swift(<5.1)
+
 #if os(Linux)
-    import Glibc
+import Glibc
 #else
-    import Darwin.C
+import Darwin.C
 #endif
 
-extension Double {
+    extension Double {
+
+        /// Exponentially scales a `Double` from the given `source` to the given
+        /// `destination`.
+        ///
+        // FIXME: Abstract Double.scale(from:to:) over `FloatingPoint`. This requires writing a `pow(_:)`
+        // which is generic over `FloatingPoint` values.
+        public mutating func scale(
+            from source: ClosedRange<Double>,
+            toExponential destination: ClosedRange<Double>
+        )
+        {
+            let sourceWidth = source.upperBound - source.lowerBound
+            let destinationRatio = destination.upperBound / destination.lowerBound
+            let position = (self - source.lowerBound) / sourceWidth
+            self = pow(destinationRatio, position) * destination.lowerBound
+        }
+
+        /// - returns: A `Double` value scaled from the given `source` range to the
+        /// given `destination` range.
+        ///
+        // FIXME: Abstract Double.scale(from:toExponential:) over `FloatingPoint`. This requires writing
+        // a `pow(_:)`  which is generic over `FloatingPoint` values.
+        public func scaled(
+            from source: ClosedRange<Double>,
+            toExponential destination: ClosedRange<Double>
+        ) -> Double
+        {
+            var copy = self
+            copy.scale(from: source, toExponential: destination)
+            return copy
+        }
+    }
+
+#else
+
+extension Real {
 
     /// Exponentially scales a `Double` from the given `source` to the given
     /// `destination`.
@@ -22,7 +61,7 @@ extension Double {
     public mutating func scale(
         from source: ClosedRange<Double>,
         toExponential destination: ClosedRange<Double>
-    )
+        )
     {
         let sourceWidth = source.upperBound - source.lowerBound
         let destinationRatio = destination.upperBound / destination.lowerBound
@@ -38,10 +77,12 @@ extension Double {
     public func scaled(
         from source: ClosedRange<Double>,
         toExponential destination: ClosedRange<Double>
-    ) -> Double
+        ) -> Double
     {
         var copy = self
         copy.scale(from: source, toExponential: destination)
         return copy
     }
 }
+
+#endif
