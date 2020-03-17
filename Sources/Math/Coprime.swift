9192f8e4042af5d885f21bad066a59c8babf782e
James Bean <jbean46@gmail.com>
2020-03-17 10:38:41 -0700

    Add coprime(_:_:)

---
+++ Sources/Math/Coprime.swift
@@ -0,0 +1,11 @@
+//
+//  Coprime.swift
+//  Math
+//
+//  Created by James Bean on 3/17/20.
+//
+
+/// - Returns: `true` if the given values are coprime. Otherwise, `false`.
+public func coprime <I: BinaryInteger> (_ a: I, _ b: I) -> Bool {
+    return gcd(a,b) == 1
+}
